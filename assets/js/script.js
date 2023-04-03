94ae0bf57d666d72657def2af082480eb663d32a
Ox Noj <geneveva@gmail.com>
2023-04-03 15:47:49 -0700

    timer/start button functionality edit and questions added to array, removing from html

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,10 +12,31 @@
 //var userInput;
 var correct = 0;
 var wrong = 0
-var questionid = 0; //need to know where in sequence of question
+//var questionid = 0; //need to know where in sequence of question
 
 var timerE1 = document.getElementById('count') 
 var mainE1 = document.getElementById('countdown')
+var tennisQ = [
+    {
+        question1:"This Legend, known as the GOAT, has won 23 Grand Slams." ,
+        answer1: "Martina Navratilova",
+        answer2: "Steffi Graf",
+        answer3: "Serena Williams",
+        answer4: "Helen Wills Moody",
+        answer5: "Venus Williams",
+        correctA: "Serena Williams"
+    },
+    {
+        question1:"This Legend, known as the GOAT, has won 23 Grand Slams." ,
+        answer1: "Martina Navratilova",
+        answer2: "Steffi Graf",
+        answer3: "Serena Williams",
+        answer4: "Helen Wills Moody",
+        answer5: "Venus Williams",
+        correctA: "Serena Williams"  
+    }
+
+]
 
 //var message = 'Time is up!';
 //var words = message.split(' ');
@@ -38,17 +59,16 @@ var mainE1 = document.getElementById('countdown')
 
 //timer countdown from 60 seconds
 function countDown(){
-    document.getElementById("startbtn").addEventListener("click", function() {
     var timeLeft = 60;
     var downloadTimer = setInterval(function function1() {
-        document.getElementById("count").innerHTML = timeLeft;
+        // document.getElementById("count").innerHTML = timeLeft;
 
-        timeLeft -=1;
-        if(timeLeft <= 0){
-            clearInterval(downloadTimer);
-            document.getElementById("count").innerHTML;
-        }
-        }, 1000);
+        // timeLeft -=1;
+        // if(timeLeft <= 0){
+        //     clearInterval(downloadTimer);
+        //     document.getElementById("count").innerHTML;
+        // }
+        // }, 1000);
 
     //setInterval used to call function to be executed every 1000 milliseconds
     //var timeInterval = setInterval(function () {
@@ -78,59 +98,75 @@ function displayMessage() {
         }
     }, 1000);
 }
-countDown();
+function programStart() {
+    countDown();
+    generateQuestion();
+}
+document.getElementById("startbtn").addEventListener("click", programStart)
+//countDown();
 
 //function timerReset(){
    // document.getElementById("countdown").reset();
 //}
 
 //questions and answers- validatation
-function questionAnswer(event){
-    var userInput = event.target.value;
-    console.log(userInput);
-    if (userInput === "Serena Williams"){
-        correct++;
-    } else {
-        wrong++;
-    } 
-    if (questionid == 0) hideShow("one", "two")}//after one is clicked, it jumps to second div
-    {
-    if (userInput === "Court"){
-        correct++;
-    } else {
-        wrong++;
-    }
-    if (questionid == 1) hideShow("two", "three")}
-    {
-    if (userInput === "Australian Open"){
-        correct++;
-    } else {
-        wrong++;
-    }
-    if (questionid == 2) hideShow("three", "four")}
-    {
-    if (userInput === "Queens"){
-        correct++;
-    } else {
-        wrong++;
-    }
-    if (questionid == 3) hideShow("four", "five")}
-    {
-    if (userInput === "France, Lacoste shirt"){
-        correct++;
-    } else {
-        wrong++;
-    }
-    if (questionid == 4) hideShow("five", "six")
-    };
+// function questionAnswer(event){
+//     var userInput = event.target.value;
+//     console.log(userInput);
+//     console.log(event);
+//     if (userInput === "Serena Williams"){
+//         correct++;
+//         document.getElementById("one").style.display = "none";
+//         document.getElementById("two").style.display = "block";
+//     } else {
+//         wrong++;
+//         document.getElementById("one").style.display = "none";
+//         document.getElementById("two").style.display = "block";
+//     }  
+//     if (userInput === "Court"){
+//         correct++;
+// 	  document.getElementById("two").style.display = "none";
+//         document.getElementById("three").style.display = "block";
+//     } else {
+//         wrong++;
+// 	  document.getElementById("two").style.display = "none";
+//         document.getElementById("three").style.display = "block";
+//     }
+//      if (userInput === "Australian Open"){
+//         correct++;
+//         document.getElementById("three").style.display = "none";
+//         document.getElementById("four").style.display = "block";
+//      } else {
+//         wrong++;
+//         document.getElementById("three").style.display = "none";
+//         document.getElementById("four").style.display = "block";
+//     }
+//      if (userInput === "Queens"){
+//         correct++;
+//         document.getElementById("four").style.display = "none";
+//         document.getElementById("five").style.display = "block";
+//     } else {
+//         wrong++;
+//         document.getElementById("four").style.display = "none";
+//         document.getElementById("five").style.display = "block";
+//     }
+//      if (userInput === "France, Lacoste shirt"){
+//         correct++;
+//         document.getElementById("five").style.display = "none";
+//         document.getElementById("six").style.display = "block";
+//     } else {
+//         wrong++;
+//         document.getElementById("five").style.display = "none";
+//         document.getElementById("six").style.display = "block" ; 
+// }}
    
 // show the next div after a question is asnwered
     //this function shows and hides the questions after a user answers
-function hideShow(currentid, nextid) {
-    var currentContainer = document.getElementById(currentid);
-    currentContainer.hidden = true;
-    var nextContainer = document.getElementById(nextid);
-    nextContainer.hidden = false;
-    questionid++;
-    console.log(questionid)
-}
+//function hideShow(currentid, nextid) {
+    //var currentContainer = document.getElementById(currentid);
+   // currentContainer.hidden = true;
+    //var nextContainer = document.getElementById(nextid);
+    //nextContainer.hidden = false;
+    //questionid++;
+    //console.log(questionid);
+//}
