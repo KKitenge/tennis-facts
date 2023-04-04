424b89e7623665569fd07cefd548f8a37824f0fe
Ox Noj <geneveva@gmail.com>
2023-04-03 20:07:49 -0700

    all questions appear in container

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,7 @@ var words = message.split(' ');
 var timerE1 = document.getElementById("count"); 
 var mainE1 = document.getElementById("countdown");
 var tennisQuiz = document.getElementById("tennisQuiz");
+var quizContainer = document.getElementById("mainQuiz-container")
 var endResult = document.getElementById("results");
 var firstChoice = document.getElementById("choice1");
 var secondChoice = document.getElementById("choice2");
@@ -19,27 +20,27 @@ var fifthChoice = document.getElementById("choice5");
 var tennisQ = [
     {
         question:"This Legend, known as the GOAT, has won 23 Grand Slams.",
-        answers: ["Martina Navratilova", "Helen Wills Moody", "Serena Williams", "Steffi Graf", "Venus Williams"]
+        answers: ["Martina Navratilova", "Helen Wills Moody", "Serena Williams", "Steffi Graf", "Venus Williams"],
         correctA: "Serena Williams"
     },
     {
         question:"Tennis is played on this type of surface:",
-        answers: ["Pitch", "Court", "Track", "Field", "Course"]
+        answers: ["Pitch", "Court", "Track", "Field", "Course"],
         correctA: "Court"  
     },
     {
         question:"This tournament is known as the Happy Slam:",
-        answers: ["Australian Open","French Open", "Wimbledon", "US Open", "Miami"]
+        answers: ["Australian Open","French Open", "Wimbledon", "US Open", "Miami"],
         correctA: "Australian Open"  
     },
     {
         question:"Where does the US Open take place in New York:",
-        answers: ["Brooklyn", "The Bronx", "Queens", "Manhattan", "Staten Island"]
+        answers: ["Brooklyn", "The Bronx", "Queens", "Manhattan", "Staten Island"],
         correctA: "Queens"  
     },
     {
         question:"Rene Lacoste, played for this country and was known for:",
-        answers: ["England, Nickname the Crocodile", "France, Lacoste shirt", "United States, Nickname the Rocket", "Germany, winning the Grand Slam", "Spain, the longest rank #1 period"] 
+        answers: ["England, Nickname the Crocodile", "France, Lacoste shirt", "United States, Nickname the Rocket", "Germany, winning the Grand Slam", "Spain, the longest rank #1 period"],
         correctA: "QFrance, Lacoste shirt" 
     }
 ];
@@ -80,7 +81,19 @@ function displayMessage() {
 function generateQuestion() {
     tennisQuiz.textContent = tennisQ[questionid].question;
     document.getElementById("mainQuiz-container").removeAttribute("hidden");
-   
+    for (let i of tennisQ) {
+        let quizbody = document.createElement("div");
+        quizbody.classList.add("quizbody");
+
+        let question = document.createElement("p");
+        question.classList.add("question");
+        question.innerHTML = i.question;
+        quizbody.appendChild(question);
+
+        for (let x = 0; x < i.answers.length; x++) {
+            quizbody.innerHTML += `<button id="question-option" class="option-div" onclick="checkAnswer(this)">${i.answers[x]}</button>`;
+        }
+        quizContainer.appendChild(quizbody);
     }
 }
 
